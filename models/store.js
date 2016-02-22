f3f730d35be8aa10f428a38ff6f9028e511c9cf7
Aaron Kosel <a_kosel@yahoo.com>
2016-02-22 03:33:47 +0000

    Remove hardcoded path

--- models/store.js
+++ models/store.js
@@ -4,7 +4,7 @@
  */
 var MegalinkList = require('./megalink_list');
 
-var store = new MegalinkList('/home/pi/megalinks/megalinks.json');
+var store = new MegalinkList(process.env.MEGALINK_PATH);
 store.fetch();
 
 module.exports = store;
